3d50560ae6101ef22385e15bf5f3086d518f47a3
newonfire <379085392@qq.com>
2018-03-20 17:52:32 +0800

    添加ignore

--- decision-drools/src/test/java/decision/drools/DecisionDroolsApplicationTests.java
+++ decision-drools/src/test/java/decision/drools/DecisionDroolsApplicationTests.java
@@ -2,16 +2,18 @@ package decision.drools;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DecisionDroolsApplicationTests {
-
+	Logger log = LoggerFactory.getLogger(DecisionDroolsApplicationTests.class);
 	@Test
 	public void contextLoads() {
-		System.out.println(11111);
+		log.info("11111");
 	}
 
 }
